9b6a544a70cfad48d61d8d737ad68b5ade5b0006
Joakim Sørensen <ludeeus@users.noreply.github.com>
2018-08-31 01:00:29 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,12 @@ with open("README.md", "r") as fh:
     LONG = fh.read()
 setuptools.setup(
     name="pyaftership",
-    version="0.0.2",
+    version="0.0.3",
     author="Joakim Sorensen @ludeeus",
     author_email="ludeeus@gmail.com",
     description="A module to get information pending parcels.",
     long_description=LONG,
+    install_requires=['requests'],
     long_description_content_type="text/markdown",
     url="https://github.com/ludeeus/pyaftership",
     packages=setuptools.find_packages(),
